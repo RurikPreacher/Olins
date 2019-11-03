f2ce5e5afbc8812038aa8d7a6fbd9ef2fa0b7536
RurikPreacher <RurikPreacher@gmail.com>
2019-11-04 00:42:58 +0200

    Made section skills_and_team

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ $(document).ready(function(){
     $('.top_slider').slick({
 		
     });
+
+    $('.team_slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      // arrows: false,
+      appendArrows: '.arrows',
+    });
   });
