c9c126f1297925b93f57070a11ca9a4dab533b21
RurikPreacher <RurikPreacher@gmail.com>
2020-02-05 00:42:13 +0200

    Finally, the hands reached the adaptive

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    /* Mobile Menu */
+    $('.mobile_nav').on('click', function() {
+      $('.mobile_navigation_box').show();
+      $(this).hide();
+      $('.mobile_nav_close').show();
+    });
+    $('.mobile_nav_close').on('click', function() {
+      $('.mobile_navigation_box').hide();
+      $(this).hide();
+      $('.mobile_nav').show();
+    });
+
     $('.top_slider').slick({
 		
     });
@@ -9,5 +21,28 @@ $(document).ready(function(){
       slidesToScroll: 1,
       // arrows: false,
       appendArrows: '.arrows',
+      responsive: [
+        {
+          breakpoint: 1320,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 1000,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+      ]
     });
   });
